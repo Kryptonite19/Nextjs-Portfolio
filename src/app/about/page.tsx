855dae34a458eaf7b7cdaeb2cae8188a2167fc56
Kryptonite19 <oguzhankeles199813@gmail.com>
2024-01-22 14:19:11 +0300

    Some fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,31 +1,5 @@
-import ProjectCard, { type ProjectCardProps } from "@/components/ProjectCard";
-export const projectData: ProjectCardProps[] = [
-  {
-    title: "Pharma App",
-    description: "A pharmacy stock management app made with Flutter",
-    details:
-      "The Pharmacy Stock Management App is a comprehensive solution designed to streamline and enhance the stock management processes within a pharmacy or pharmaceutical business. Developed using the Flutter framework, this app provides a modern and user-friendly interface, ensuring efficient inventory control and improved operational workflows.",
-    technologies: "Flutter",
-    link: "string",
-  },
-  {
-    title: "Bot Mahir",
-    description: "A Discord chat bot project to have fun with friends",
-    details:
-      "Bot Mahir is a versatile and interactive application developed using Python, specifically designed to enhance the functionality and engagement within Discord servers. This bot serves as a powerful assistant, automating various tasks, providing real-time information, and facilitating communication between users. With its customizable features and ease of integration, Bot Mahir is a valuable addition to any Discord community.",
-    technologies: "Python",
-    link: "string",
-  },
-  {
-    title: "OGZ Cuisine",
-    description:
-      "A delightful recipe app that provides a diverse range of recipes from various categories.",
-    details:
-      "OGZ Cuisine is a captivating and user-friendly platform designed to elevate the cooking and dining experience. Developed using the React framework, this app offers a delightful and interactive way for users to discover, save, and share a diverse range of recipes across various categories. With a clean and modern interface, OGZ Cuisine aims to inspire both novice and seasoned cooks to explore new culinary horizons.",
-    technologies: "React",
-    link: "string",
-  },
-];
+import ProjectCard from "@/components/ProjectCard";
+import { projectData } from "@/data/ProjectData";
 
 const About = () => {
   return (
